2df0aa87fa479a33ca6ce905a06515f424601a86
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-03-02 22:35:52 +0530

    Added instrumental test

--- app/src/androidTest/java/com/example/userinfo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/userinfo/ExampleInstrumentedTest.kt
@@ -47,4 +47,39 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.addressEt))
             .check(ViewAssertions.matches(ViewMatchers.withText("Hyderabad")))
     }
+
+    @Test
+    fun testToDisplayMessageAfterConfirmButton() {
+        onView(withId(R.id.userNameEt)).perform(typeText("Bhavana"), closeSoftKeyboard())
+        onView(withId(R.id.emailEt)).perform(typeText("bhavana@gmail.com"),closeSoftKeyboard())
+        onView(withId(R.id.numberEt)).perform(typeText("1234567893"),closeSoftKeyboard())
+        onView(withId(R.id.pincodeEt)).perform(typeText("123456"),closeSoftKeyboard())
+        onView(withId(R.id.addressEt)).perform(typeText("Hyderabad"),closeSoftKeyboard())
+        onView(withId(R.id.buttonLabel)).perform(ViewActions.click())
+        onView(withId(R.id.confirmButton)).perform(ViewActions.click())
+        onView(withId(R.id.displayUserInfo)).check(ViewAssertions.matches(ViewMatchers.withText("Hi "+"Bhavana"+", How are you? Are you staying at "+"Hyderabad"+"-"+"123456"+"code. I am not able to contact you on "+"1234567893"+". Can I email you the details at "+"bhavana@gmail.com")))
+
+    }
+
+    @Test
+    fun testToDisplayFieldsAfterCancelButton(){
+        onView(withId(R.id.userNameEt)).perform(typeText("Bhavana"), closeSoftKeyboard())
+        onView(withId(R.id.emailEt)).perform(typeText("bhavana@gmail.com"),closeSoftKeyboard())
+        onView(withId(R.id.numberEt)).perform(typeText("1234567893"),closeSoftKeyboard())
+        onView(withId(R.id.pincodeEt)).perform(typeText("123456"),closeSoftKeyboard())
+        onView(withId(R.id.addressEt)).perform(typeText("Hyderabad"),closeSoftKeyboard())
+        onView(withId(R.id.buttonLabel)).perform(ViewActions.click())
+        onView(withId(R.id.cancelButton)).perform(ViewActions.click())
+        onView(withId(R.id.userNameEt))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Bhavana")))
+        onView(withId(R.id.emailEt))
+            .check(ViewAssertions.matches(ViewMatchers.withText("bhavana@gmail.com")))
+        onView(withId(R.id.numberEt))
+            .check(ViewAssertions.matches(ViewMatchers.withText("1234567893")))
+        onView(withId(R.id.pincodeEt))
+            .check(ViewAssertions.matches(ViewMatchers.withText("123456")))
+        onView(withId(R.id.addressEt))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Hyderabad")))
+    }
+
 }
