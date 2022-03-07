6b652aa8e7ae8f98bbc1e379d0102d19786aadda
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-03-07 12:04:49 +0530

    Used binding and groups

--- app/src/main/java/com/example/userinfo/DisplayUserInfoActivity.kt
+++ app/src/main/java/com/example/userinfo/DisplayUserInfoActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class DisplayUserInfoActivity:AppCompatActivity() {
+class DisplayUserInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_user)
-        val getUserName=intent.getStringExtra(USERNAME)
-        val getEmail=intent.getStringExtra(EMAIL)
-        val getPhoneNumber=intent.getStringExtra(PHONENUMBER)
-        val getPincode=intent.getStringExtra(PINCODE)
-        val getAddress=intent.getStringExtra(ADDRESS)
-        var outputString = getString(R.string.displayMessage, getUserName, getAddress,getPincode,getPhoneNumber,getEmail)
+        val getUserName = intent.getStringExtra(USERNAME)
+        val getEmail = intent.getStringExtra(EMAIL)
+        val getPhoneNumber = intent.getStringExtra(PHONENUMBER)
+        val getPincode = intent.getStringExtra(PINCODE)
+        val getAddress = intent.getStringExtra(ADDRESS)
+        var outputString = getString(R.string.displayMessage, getUserName, getAddress, getPincode,getPhoneNumber, getEmail)
+
         findViewById<TextView>(R.id.displayUserInfo).setText(outputString)
     }
 }
--- app/src/main/java/com/example/userinfo/MainActivity.kt
+++ app/src/main/java/com/example/userinfo/MainActivity.kt
@@ -1,104 +1,61 @@
 package com.example.userinfo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.userinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var validation:Validation
-    var visibility=true
+    private lateinit var validation: Validation
+    private lateinit var binding: ActivityMainBinding
+
+    var visibility: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         validation = Validation()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val userName = findViewById<EditText>(R.id.userNameEt)
-        val email = findViewById<EditText>(R.id.emailEt)
-        val number = findViewById<EditText>(R.id.numberEt)
-        val pincode = findViewById<EditText>(R.id.pincodeEt)
-        val address = findViewById<EditText>(R.id.addressEt)
-        val validate = findViewById<Button>(R.id.buttonLabel)
-        val cancel=findViewById<Button>(R.id.cancelButton)
-        val confirm=findViewById<Button>(R.id.confirmButton)
+        val validate = binding.validateButton
 
         validate.setOnClickListener() {
-            val isAllFieldsChecked = checkAllFields(userName.text.toString(),email.text.toString(),number.text.toString(),pincode.text.toString(),address.text.toString())
-            if (isAllFieldsChecked) {
-                val validateResult=validateAllFields(userName.text.toString(),email.text.toString(),number.text.toString(),pincode.text.toString())
-                if (validateResult) {
-                    visibility=false
-                    modifyVisibility(userName,email,number,pincode,address,validate,cancel,confirm,visibility)
-                }
-            }
-            confirm.setOnClickListener() {
-                if (isAllFieldsChecked) {
-                    val validateResult=validateAllFields(userName.text.toString(),email.text.toString(),number.text.toString(),pincode.text.toString())
-                    if (validateResult) {
-                        val intent = Intent(this, DisplayUserInfoActivity::class.java)
-                        intent.putExtra(USERNAME, userName.text.toString())
-                        intent.putExtra(EMAIL, email.text.toString())
-                        intent.putExtra(PHONENUMBER, number.text.toString())
-                        intent.putExtra(PINCODE, pincode.text.toString())
-                        intent.putExtra(ADDRESS, address.text.toString())
-                        startActivity(intent)
-                    }
-                }
-            }
-            cancel.setOnClickListener() {
-                visibility=true
-                modifyVisibility(userName, email, number, pincode, address,validate,cancel, confirm, visibility)
+            val validateResult = checkAndValidateFields(binding.userNameEt.text.toString(),
+                binding.emailEt.text.toString(),
+                binding.numberEt.text.toString(),
+                binding.pincodeEt.text.toString(),
+                binding.addressEt.text.toString())
+            if (validateResult) {
+                visibility = false
+                modifyVisibility( validate, visibility)
             }
         }
-
+        binding.confirmButton.setOnClickListener() {
+            val intent = Intent(this, DisplayUserInfoActivity::class.java)
+            intent.putExtra(USERNAME, binding.userNameEt.text.toString())
+            intent.putExtra(EMAIL, binding.emailEt.text.toString())
+            intent.putExtra(PHONENUMBER, binding.numberEt.text.toString())
+            intent.putExtra(PINCODE, binding.pincodeEt.text.toString())
+            intent.putExtra(ADDRESS, binding.addressEt.text.toString())
+            startActivity(intent)
+        }
+        binding.cancelButton.setOnClickListener() {
+            visibility = true
+            modifyVisibility( validate, visibility)
+        }
     }
 
-    fun validateAllFields(
-        userName: String,
-        email: String,
-        number: String,
-        pincode: String,
-    ): Boolean {
-
-        if (!validation.isValidEmail(email)) {
-            callToast( "Invalid Email Address")
-            return false
-        }
-        if (!validation.isValidPinCode(pincode)) {
-            callToast("Invalid Pincode")
-            return false
-        }
-        if (!validation.isValidPhone(number)) {
-            callToast("Invalid Phone Number")
-            return false
-        }
-        if(!validation.isValidUserName(userName)){
-            callToast("Invalid User Name")
-            return false
-        }
-        return true
-    }
-
-     fun callToast(str:String){
-        Toast.makeText(applicationContext, str,
-            Toast.LENGTH_SHORT).show()
-    }
-
-    fun checkAllFields(
+    fun checkAndValidateFields(
         userName: String,
         email: String,
         number: String,
         pinCode: String,
-        address: String
+        address: String,
     ): Boolean {
-
         if (userName.trim().isEmpty()) {
-            Log.i("UserName", userName)
             callToast("Username shouldn't be empty")
             return false
         }
@@ -118,46 +75,47 @@ class MainActivity : AppCompatActivity() {
             callToast("Address shouldn't be empty")
             return false
         }
+        if (!validation.isValidUserName(userName)) {
+            callToast("Invalid User Name")
+            return false
+        }
+        if (!validation.isValidEmail(email)) {
+            callToast("Invalid Email Address")
+            return false
+        }
+        if (!validation.isValidPhone(number)) {
+            callToast("Invalid Phone Number")
+            return false
+        }
+        if (!validation.isValidPinCode(pinCode)) {
+            callToast("Invalid Pincode")
+            return false
+        }
         return true
     }
 
+    fun callToast(str: String) {
+        Toast.makeText(applicationContext, str,
+            Toast.LENGTH_SHORT).show()
+    }
+
     fun modifyVisibility(
-        userName:EditText,
-        email: EditText,
-        number: EditText,
-        pinCode: EditText,
-        address: EditText,
-        validate:Button,
-        cancel:Button,
-        confirm:Button,
-        visibility: Boolean
-    ){
-        userName.isEnabled=visibility
-        email.isEnabled=visibility
-        pinCode.isEnabled=visibility
-        number.isEnabled=visibility
-        address.isEnabled=visibility
-        validate.visibility = if(!visibility) View.GONE else View.VISIBLE
-        cancel.visibility = if(visibility) View.GONE else View.VISIBLE
-        confirm.visibility =if(visibility) View.GONE else View.VISIBLE
+        validate: Button,
+        visibility: Boolean,
+    ) {
+        binding.groupView.isEnabled=visibility
+        validate.visibility = if (!visibility) View.GONE else View.VISIBLE
+        binding.groupButton.visibility = if (visibility) View.GONE else View.VISIBLE
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean("visibility",visibility)
+        outState.putBoolean("visibility", visibility)
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val v=savedInstanceState.getBoolean("visibility")
-        val userName = findViewById<EditText>(R.id.userNameEt)
-        val email = findViewById<EditText>(R.id.emailEt)
-        val number = findViewById<EditText>(R.id.numberEt)
-        val pincode = findViewById<EditText>(R.id.pincodeEt)
-        val address = findViewById<EditText>(R.id.addressEt)
-        val validate = findViewById<Button>(R.id.buttonLabel)
-        val cancel=findViewById<Button>(R.id.cancelButton)
-        val confirm=findViewById<Button>(R.id.confirmButton)
-        modifyVisibility(userName,email, number, pincode, address,validate, cancel, confirm,v)
+        val v = savedInstanceState.getBoolean("visibility")
+        modifyVisibility(binding.validateButton, v)
     }
 }
